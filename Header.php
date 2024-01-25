77cacd46a0e20ce367f3795ab09a6e26d24f1352
Parizetm <parizetm@free.fr>
2024-01-25 18:55:11 +0100

    Initial commit

---
+++ Header.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-info fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="media/genshinvavf_logo.ico" alt=""
+                style="width: 40px; margin-inline: auto" /></a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php" aria-current="page">Accueil <span
+                            class="visually-hidden">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item dropdown ml-3">
+                    <a class="nav-link dropdown-toggle active" href="#" id="dropdownId" data-bs-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Plus</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="A propos.php">Qui sommes-nous ?</a>
+                        <a class="dropdown-item" href="Bandes rythmo.php">Bandes rythmo</a>
+                        <a class="dropdown-item" href="Demande bande rythmo.php">Demande bande rythmo</a>
+                        <a class="dropdown-item" href="Depot audition.php">Dépot audition</a>
+                        <a class="dropdown-item" href="actualites.php">Actualités</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="d-flex my-2 my-lg-0">
+                <input class="form-control me-sm-2" type="text" placeholder="Search" />
+                <a href="search.php">
+                    <button class="btn my-2 my-sm-0" type="button" href="search.php">
+                        Search
+                    </button>
+                </a>
+            </form>
+        </div>
+    </div>
+</nav>
---
+++ Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Genshin VA VF</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" href="media/genshinvavf_logo.ico" type="image/x-icon" />
+</head>
+
+<body>
+    <header>
+        <?php 
+        include "header.php"
+         ?> 
+    </header>
+    <main>
+    <img class="image_top"src="media/twins1.png" alt=""/>
+
+    </main>
+
+</body>
+
+</html>
